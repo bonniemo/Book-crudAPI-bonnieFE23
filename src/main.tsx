4cc65cc82824fa60022bb16b9a79afe30b5f38b4
Bonnie <moller.bon@gmail.com>
2024-04-28 19:46:29 +0200

    wrapped router in provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Nav from './routes/Nav'
-import NotFound from './routes/NotFound'
-import Home from './routes/Home'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Nav from "./routes/Nav";
+import NotFound from "./routes/NotFound";
+import Home from "./routes/Home";
+import GlobalContextProvider from "./components/GlobalContextProvider";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Nav/>,
-    errorElement: <NotFound/>,
+    element: <Nav />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
-        element: <Home/>
-      }
-    ]
-  }
-])
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <GlobalContextProvider>
+      <RouterProvider router={router} />
+    </GlobalContextProvider>
+  </React.StrictMode>
+);
