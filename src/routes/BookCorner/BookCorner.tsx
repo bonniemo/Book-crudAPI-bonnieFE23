fb3a64a4f2b4d237bb42816c6020f4bbc1b98eec
Bonnie <moller.bon@gmail.com>
2024-05-09 20:14:22 +0200

    styles

--- src/routes/BookCorner/BookCorner.tsx
+++ src/routes/BookCorner/BookCorner.tsx
@@ -6,7 +6,7 @@ import readingHistory from '../../Assets/history-book.png'
 const BookCorner = () => {
   return (
     <article className="flex row-auto">
-        <nav className="border-8 border-fuchsia-50 flex p-5 mt-5 w-64">
+        <nav className=" border-r-8 border-fuchsia-50 flex p-5 w-48">
             <ul className="flex flex-col gap-3 mt-5">
                 <li className="mb-5"><NavLink className="" to="/BookCorner/FavouriteBooks"><img src={fav} className="mx-2"/>My Favourite Books</NavLink></li>
                 <li className="mb-5"><NavLink className="" to="/BookCorner/FavouriteAuthors"><img src={author} className="mx-2"/>My Favourite Authors</NavLink></li>
--- src/routes/Nav.tsx
+++ src/routes/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <>
       <nav className="flex justify-between items-center bg-fuchsia-50">
-        <h1 className="mx-2 text-3xl font-serif italic">BookHaven</h1>
+        <h1 className="mx-4 text-5xl font-serif italic">BookHaven</h1>
         <ul className="flex gap-5 justify-end p-4">
           <li><NavLink to ="/" className="flex flex-col items-center hover:font-bold"><img src={iconHome} className="w-8"/>Home</NavLink></li>
           <li><NavLink to ="/BookCorner" className="flex flex-col items-center hover:font-bold"><img src={iconFavourites} className="w-8"/>My Book Corner</NavLink></li>
